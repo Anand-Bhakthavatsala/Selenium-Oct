5f37a6a8c137b4fc54186a9f96a65a1b89b23d1c
Anand-Bhakthavatsala <35788456+Anand-Bhakthavatsala@users.noreply.github.com>
2020-10-20 11:22:51 +0530

    Added the code

--- SeleniumJava-Oct/src/JavaPrograms/ForLoop.java
+++ SeleniumJava-Oct/src/JavaPrograms/ForLoop.java
@@ -4,7 +4,16 @@ public class ForLoop {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		for(int num=10;num>=1;num--)//initialize;condition;increment/decrement
+		{
+			System.out.println("Value of num "+num);
+			try {
+				Thread.sleep(2000);//we are instructing the program to stop for 2 second
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}//1000 milli seconds is 1 second
+		}
 
 	}
-
 }
